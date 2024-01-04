3c8e46004245eadff0953f8b6e2e01eeb58c8f39
riya2641 <riya2641@CSLH2313.global.chainsys.com>
2024-01-04 17:26:41 +0530

    Test Commit

--- src/main/java/in/riyasahamed/MovieticketappApplication.java
+++ src/main/java/in/riyasahamed/MovieticketappApplication.java
@@ -1,3 +1,17 @@
+/*
+ * FileName: MovieticketappApplication.java
+ * Author  : riya2641
+ * 
+ * Using JRE 17.0.4.1
+ * 
+ * Copyright(c) 2024 Chain-Sys Corporation Inc.
+ * Duplication or distribution of this code in part or in whole by any media
+ * without the express written permission of Chain-Sys Corporation or its agents is
+ * strictly prohibited.
+ * 
+ * REVISION         DATE            NAME     DESCRIPTION
+ * 511.101       04-Jan-2024       Riyas Ahamed   Initial Code
+ */
 package in.riyasahamed;
 
 import org.springframework.boot.SpringApplication;
@@ -11,8 +25,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 public class MovieticketappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieticketappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MovieticketappApplication.class, args);
+    }
 
 }
